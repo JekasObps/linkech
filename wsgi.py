e1f6cce951a2bc0e0f8a8d0f3f10d75f3167eaca
JekasObps <jekas.obps@gmail.com>
2021-01-26 21:07:15 +0200

    +

--- wsgi.py
+++ wsgi.py
@@ -2,5 +2,5 @@ from chat import create_socketio_app
 
 application, socket = create_socketio_app()
 
-# if __name__ == '__main__':
-#     socket.run(application)
+if __name__ == '__main__':
+    socket.run(application)
